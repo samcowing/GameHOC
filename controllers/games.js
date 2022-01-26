2dbd28083314d2be5f02746144c0043d5bad199c
joshhovis <joshuahovis3@gmail.com>
2022-01-26 15:12:41 -0500

    Add some styling for games/show.ejs mobile view

--- controllers/games.js
+++ controllers/games.js
@@ -20,19 +20,10 @@ apiQueryParams = {
 /*****************************/
 function categorySelect(type, id = '0') {
     let requestURL = ''
-<<<<<<< HEAD
     console.log('type:', type)
     switch (type) {
         case ('genres'):
-            if (id === '') {
-=======
-        console.log('type:',type)
-    switch(type)
-    {
-        case('genres'):
-            if (id === '0')
-            {
->>>>>>> main
+            if (id === '0') {
                 requestURL = "https://api.rawg.io/api/games?key=b37c07aab35b44058235af257c65be19"
             } else {
                 requestURL = "https://api.rawg.io/api/games?key=b37c07aab35b44058235af257c65be19" + "&" + type + "=" + id
