b0f6f0b11233392d99fe40b2949483d9f01ba023
joshhovis <joshuahovis3@gmail.com>
2022-01-27 01:05:32 -0500

    Add styling for games/show.ejs mobile view and tweak some games/index.ejs styling

---
+++ views/helpers.js
@@ -0,0 +1,13 @@
+// function toggleText() {
+//     const shortDescription = document.getElementById('description')
+//     const fullDescription = document.getElementById('desc-full')
+//     if (fullDescription.style.display === "none") {
+//         shortDescription.style.display = "none"
+//         fullDescription.style.display = " "
+//     } else {
+//         fullDescription.style.display = "none"
+//         shortDescription.style.display = " "
+//     }
+// }
+
+// module.exports = toggleText
