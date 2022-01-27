b4d4096f0d2ac705cf954647fab07d211a2e75e8
sam-cowing <sam.ryan.cowing@gmail.com>
2022-01-27 13:01:56 -0500

    moved helper

---
+++ controllers/helpers.js
@@ -0,0 +1,21 @@
+function toggleText() {
+     const shortDescription = document.getElementById('description')
+     const fullDescription = document.getElementById('desc-full')
+     if (fullDescription.style.display === "none") {
+         shortDescription.style.display = "none"
+         fullDescription.style.display = " "
+     } else {
+         fullDescription.style.display = "none"
+         shortDescription.style.display = " "
+     }
+}
+
+
+
+function showFull() {
+    document.getElementById("description").style.display = "none"
+    document.getElementById("desc-full").style.display = "block"
+}
+
+
+module.exports = showFull
